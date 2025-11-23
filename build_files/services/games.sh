3a21445720fa946b14d52b2371ee8fa46c40f031
Hasenfresser <robert.werner@mailbox.org>
2025-11-23 01:18:46 +0100

    Games disk decryption finally fixed (hopefully 7)

--- build_files/services/games.sh
+++ build_files/services/games.sh
@@ -4,8 +4,9 @@
 M2=games_m2
 HDD=games_hdd
 
-DEV_M2=/dev/disk/by-uuid/bfc2b949-6458-47e6-a9c3-9d7d7e812522
-DEV_HDD=/dev/disk/by-uuid/af768f9e-b984-4185-8684-75e41a3daeeb
+DEV=/dev/disk/by-uuid/
+DEV_M2=$DEV"bfc2b949-6458-47e6-a9c3-9d7d7e812522"
+DEV_HDD=$DEV"af768f9e-b984-4185-8684-75e41a3daeeb"
 
 KEY=/root/
 KEY_M2=$KEY$M2
@@ -23,9 +24,11 @@ MNT_HDD=$MNT"Linux-Games-HDD"
 mkdir -p $MNT_M2 $MNT_HDD || exit 1
 
 [ -e $DM_M2 ] || cryptsetup open $DEV_M2 --key-file=$KEY_M2 $M2 || exit 10
+sleep 5
 mountpoint -q $MNT_M2 || mount -o defaults,noatime $DM_M2 $MNT_M2 || exit 11
 
-[ -e $DM_HDD ] || cat $KEY_HDD | cryptsetup open --type bitlk $DEV_HDD $HDD || exit 20
+[ -e $DM_HDD ] || (cat $KEY_HDD | cryptsetup open --type bitlk $DEV_HDD $HDD) || exit 20
+sleep 5
 mountpoint -q $MNT_HDD || mount -o defaults $DM_HDD $MNT_HDD || exit 21
 
 echo All games disks decrypted and mounted!
